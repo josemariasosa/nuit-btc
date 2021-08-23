d1dd493124176e793856d8b891d8e713dc4a1524
José María Sosa Resendiz <jose.skedar@gmail.com>
2021-08-23 15:35:14 -0500

    removing base58 from utils

--- app/key/utils.py
+++ app/key/utils.py
@@ -5,12 +5,11 @@ import re
 import hashlib
 import hmac
 
-from crypto.ecdsa.secp256k1 import N, G, S256Point
+from crypto.ecdsa.secp256k1 import N, G, S256Point, PrivateKey
 
 ZERO = b'\x00'
 HARDENED_INDEX = (2 ** 32) // 2
 REGEX_DERIVATION_PATH = re.compile("^m(/[0-9]+['hH]?)*$")
-BASE58_ALPHABET = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
 
 
 def _generate_public_key(private_key: bytes) -> bytes:
@@ -48,33 +47,17 @@ def _derive_public_child(pubkey: bytes, chaincode: bytes,
     return child_pubkey, R256
 
 
+def _pubkey_to_address(pubkey: bytes, testnet: bool = False) -> str:
+    pubkey_point = S256Point.parse(pubkey)
+    return pubkey_point.address(compressed=True, testnet=testnet)
+
+
+def _privkey_to_wif(privkey: bytes, testnet: bool = False) -> str:
+    secret = int.from_bytes(privkey, 'big')
+    return PrivateKey(secret=secret).wif(compressed=True, testnet=testnet)
+
+
 def _pubkey_to_fingerprint(pubkey):
     rip = hashlib.new("ripemd160")
     rip.update(hashlib.sha256(pubkey).digest())
     return rip.digest()[:4]
-
-
-# Refactored code segments from <https://github.com/keis/base58>
-def _b58encode_extended_key(v: bytes) -> str:
-    p, acc = 1, 0
-    for c in reversed(v):
-        acc += p * c
-        p = p << 8
-
-    string = ""
-    while acc:
-        acc, idx = divmod(acc, 58)
-        string = BASE58_ALPHABET[idx : idx + 1] + string
-    return string
-
-
-def _b58decode_extened_key(s: str) -> bytes:
-    """ Decodes the base58-encoded string s into bytes (xprv have 82 bytes) """
-    decoded = 0
-    multi = 1
-    s = s[::-1]
-    for char in s:
-        decoded += multi * BASE58_ALPHABET.index(char)
-        multi = multi * 58
-
-    return decoded.to_bytes(82, byteorder='big')
