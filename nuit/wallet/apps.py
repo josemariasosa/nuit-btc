c825d9e5a89fd0686bea45c42257ed2280d4b7c6
José María Sosa Resendiz <jose.skedar@gmail.com>
2021-09-07 09:52:17 -0500

    organizing the code

--- nuit/wallet/apps.py
+++ nuit/wallet/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class KeychainConfig(AppConfig):
+class WalletConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'keychain'
+    name = 'wallet'
