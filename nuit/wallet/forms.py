9f36b46840b59833fedf2375299e218823d1cbac
José María Sosa Resendiz <jose.skedar@gmail.com>
2024-01-14 09:34:50 -0600

    new form

--- nuit/wallet/forms.py
+++ nuit/wallet/forms.py
@@ -4,6 +4,7 @@ from django import forms
 LANGUAGES = [('en', 'Ingles'), ('es', 'Espanol')]
 NUMBER_OF_WORDS = [('13', '13'), ('15', '15'), ('18', '18'), ('21', '21'), ('24', '24')]
 
+
 class MnemonicForm(forms.Form):
     language = forms.ChoiceField(widget=forms.Select,
                                          choices=LANGUAGES)
